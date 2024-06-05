91ad2ee5f09560e97e30bfda73c81c7e6cfa91a3
sandeep0302 <gsandeepgoud0302@gmail.com>
2024-06-05 15:06:28 +0530

    Right Side

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,65 @@
 import React from 'react'
 import './Hero.css'
 import Header from '../../Header/Header'
-
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png"
+import Heart from "../../assets/heart.png"
+import Calories from '../../assets/calories.png'
 const Hero = () => {
   return (
     <div className='hero'>
       <div className='left-h'>
         <Header />
+
+        { /* the best ad */}
         <div className="the-best-ad">
           <div>
           </div>
           <span>The best fitness club in the town</span>
         </div>
+        {/* Hero Heading */}
+        <div className='hero-text'>
+          <div>
+            <span className='stroke-text'>Shape </span>
+            <span>Your</span>
+          </div>
+          <div>
+            <span>
+              Ideal body
+            </span>
+          </div>
+          <div>
+            In here we will help you to shape and build your Ideal body
+          </div>
+        </div>
+        {/* figures*/}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coaches</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+        {/*hero buttons*/}
+        <div className="hero-button">
+          <buttons className='btn'>Get Started</buttons>
+          <buttons className='btn'>Learn More</buttons>
+        </div>
       </div>
       <div className='right-h'></div>
+      <button className='btn'>Join Now</button>
+      <div className='heart-rate'>
+        <img src={Heart} alt="" />
+        <span>Heart Rate</span>
+        <span>116bpm</span>
+      </div>
     </div>
   )
 }
