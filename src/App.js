0904dc71c3541ddb0177bbad55fb5bd2511a1647
sandeep0302 <gsandeepgoud0302@gmail.com>
2024-06-06 11:54:48 +0530

    Reasons images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Hero from './components/Hero/Hero.jsx'
+import Reasons from './components/Reasons/Reasons.jsx'
 import Programs from './components/Programs/Programs.jsx'
+
 
 
 function App() {
@@ -8,6 +10,7 @@ function App() {
     <>
       <Hero />
       <Programs />
+      <Reasons />
     </>
   );
 }
