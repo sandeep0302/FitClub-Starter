25fb7d04ab8947eb1af8c794aef3cec76b5e8b23
sandeep0302 <gsandeepgoud0302@gmail.com>
2024-06-06 13:33:50 +0530

    Plans Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Hero from './components/Hero/Hero.jsx'
 import Reasons from './components/Reasons/Reasons.jsx'
 import Programs from './components/Programs/Programs.jsx'
+import Plans from './components/Plans/Plans.jsx'
 
 
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Programs />
       <Reasons />
+      <Plans />
     </>
   );
 }
--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,6 +4,10 @@ import image1 from '../../assets/image1.png';
 import image2 from '../../assets/image2.png';
 import image3 from '../../assets/image3.png';
 import image4 from '../../assets/image4.png';
+import nb from '../../assets/nb.png'
+import adidas from '../../assets/adidas.png'
+import nike from '../../assets/nike.png'
+import tick from '../../assets/tick.png'
 
 const Reasons = () => {
   return (
@@ -15,8 +19,47 @@ const Reasons = () => {
           <img src={image3} alt="" />
           <img src={image4} alt="" />
         </div>
-        <div className="right-r"></div>
+        <div className="right-r">
+          <span className='stroke-text'>some reasons</span>
+          <div>
+            <span>why </span>
+            <span>choose us ?</span>
+          </div>
+
+          <div className='details-r'>
+            <div>
+              <img src={tick} alt="" />
+              <span>OVER 140+ EXPERT COACHES</span>
+            </div>
+            <div>
+              <img src={tick} alt="" />
+              <span>TRAIN SMATER AND FASTER THAN BEFORE</span>
+            </div>
+
+            <div>
+              <img src={tick} alt="" />
+              <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+            </div>
+            <div>
+              <img src={tick} alt="" />
+              <span>RELIABLE PARTNERS</span>
+            </div>
+          </div>
+          <span style={{
+            color: 'var(--gray)',
+            fontWeight: 'normal'
+          }}  >OUR PARTNERS</span>
+          <div className="partners">
+            <img src={nb} alt="" />
+            <img src={adidas} alt="" />
+            <img src={nike} alt="" />
+          </div>
+
+
+        </div>
+
       </div>
+
     </div>
   )
 }
